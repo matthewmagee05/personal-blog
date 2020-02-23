892f939505a11abc199ae4039fee4fa5e4c2aad5
Matthew Magee <matthewmagee05@gmail.com>
2020-02-23 15:13:26 -0600

    integrated portfolio into blog

---
+++ config/website.js
@@ -0,0 +1,25 @@
+const tailwind = require('../tailwind')
+
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+
+  siteTitle: 'Matthew Magee - Portfolio', // Navigation and Site Title
+  siteTitleAlt: 'Matt', // Alternative Site title for SEO
+  siteTitleShort: 'Matt', // short_name for manifest
+  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
+  siteUrl: 'https://matthewmagee.dev', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logo.png', // Used for SEO and manifest
+  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
+  author: 'Matthew Magee', // Author for schema.org JSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@matthewmagee05', // Twitter Username
+  ogSiteName: 'matt', // Facebook Site Name
+  ogLanguage: 'en_US', // Facebook Language
+  googleAnalyticsID: 'UA-47519312-5',
+
+  // Manifest and Progress color
+  themeColor: tailwind.colors.orange,
+  backgroundColor: tailwind.colors.blue,
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,152 @@
 import React from "react"
-import { Link } from "gatsby"
+import styled from "styled-components"
+import tw from "tailwind.macro"
+import { Parallax } from "react-spring/renderprops-addons.cjs"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+// Components
+import HomeLayout from "../components/HomeLayout"
+import ProjectCard from "../components/ProjectCard"
+
+// Elements
+import Inner from "../elements/Inner"
+import { Title, BigTitle, Subtitle } from "../elements/Titles"
+
+// Views
+import Hero from "../views/Hero"
+import Projects from "../views/Projects"
+import About from "../views/About"
+import Contact from "../views/Contact"
+
+import avatar from "../images/avatar.jpg"
+import brewtodo from "../images/brewtodo.jpeg"
+import rest from "../images/rest.gif"
+import pokemon from "../images/Optimized-pokemon.jpg"
+import drum from "../images/drum.jpg"
+
+import { Link } from "gatsby"
 import Button from "../components/button"
 
-class IndexPage extends React.Component {
-  render() {
-    const siteTitle = "Gatsby Starter Personal Website"
-
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="Home"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
-        <h1>
-          Hey people{" "}
-          <span role="img" aria-label="wave emoji">
-            👋
-          </span>
-        </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
-        <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
-        </p>
-        <p>Now go build something great!</p>
-        <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
-        </Link>
-      </Layout>
-    )
+const ProjectsWrapper = styled.div`
+  ${tw`flex flex-wrap justify-between mt-8`};
+  display: grid;
+  grid-gap: 4rem;
+  grid-template-columns: repeat(2, 1fr);
+  @media (max-width: 1200px) {
+    grid-gap: 3rem;
   }
-}
+  @media (max-width: 900px) {
+    grid-template-columns: 1fr;
+    grid-gap: 2rem;
+  }
+`
 
-export default IndexPage
+const AboutHero = styled.div`
+  ${tw`flex flex-col lg:flex-row items-center mt-8`};
+`
+
+const Avatar = styled.img`
+  ${tw`rounded-full w-32 xl:w-48 shadow-lg h-auto`};
+`
+
+const AboutSub = styled.span`
+  ${tw`text-white pt-12 lg:pt-0 lg:pl-12 text-2xl lg:text-3xl xl:text-4xl`};
+`
+
+const AboutDesc = styled.p`
+  ${tw`text-grey-light text-lg md:text-xl lg:text-2xl font-sans pt-6 md:pt-12 text-justify`};
+`
+
+const ContactText = styled.p`
+  ${tw`text-grey-light font-sans text-xl md:text-2xl lg:text-3xl`};
+`
+
+const Footer = styled.footer`
+  ${tw`text-center text-grey absolute pin-b p-6 font-sans text-md lg:text-lg`};
+`
+
+const Index = () => (
+  <>
+    <HomeLayout />
+    <Parallax pages={5}>
+      <Hero offset={0}>
+        <BigTitle>
+          Hello, <br /> I'm Matthew Magee.
+        </BigTitle>
+        <Link to="/blog/">
+          <Button marginTop="35px">Check Out My Blog</Button>
+        </Link>
+        <Subtitle>
+          I'm a full stack developer creating next generation web experiences
+          for consumer-facing companies.
+        </Subtitle>
+      </Hero>
+      <Projects offset={1}>
+        <Title>Projects</Title>
+        <ProjectsWrapper>
+          <ProjectCard
+            title="BrewTodo"
+            link="https://github.com/matthewmagee05/brewtodo"
+            bg={brewtodo}
+          >
+            Keep track of your brewery escapades.
+          </ProjectCard>
+          <ProjectCard
+            title="Restaurant Finder"
+            link="https://github.com/matthewmagee05/matthew-magee-project1"
+            bg={rest}
+          >
+            ASP.NET MVC Restaurant Application
+          </ProjectCard>
+          <ProjectCard
+            title="React Native App"
+            link="https://github.com/matthewmagee05/PokeSearch"
+            bg={pokemon}
+          >
+            Gotta Catch em all!
+          </ProjectCard>
+          <ProjectCard
+            title="JavaScript Drum Kit"
+            link="https://github.com/matthewmagee05/javascript-drum-kit"
+            bg={drum}
+          >
+            Fun for all ages.
+          </ProjectCard>
+        </ProjectsWrapper>
+      </Projects>
+      <About offset={3}>
+        <Title>About</Title>
+        <AboutHero>
+          <Avatar src={avatar} alt="John Doe" />
+          <AboutSub>
+            If at first you don’t succeed; call it version 1.0
+          </AboutSub>
+        </AboutHero>
+        <AboutDesc>
+          I am a .NET web developer focusing on full stack development with C#.
+          I have created web clients with JavaScript, HTML5, CSS3, JQuery,
+          ASP.NET MVC 5 and Angular. I have implemented back-end technologies
+          including ADO.NET Entity Framework for data access, SQL Server and
+          NoSQL databases like MongoDB. In addition, I have worked in diverse
+          team environments following Agile/Scrum methodologies.
+        </AboutDesc>
+      </About>
+      <Contact offset={4}>
+        <Inner>
+          <Title>Get in touch</Title>
+          <ContactText>
+            Say <a href="mailto:contact@matthewmagee.dev">Hi</a> or find me on
+            other platforms:{" "}
+            <a href="https://www.linkedin.com/in/mmagee05/">LinkedIn</a> &{" "}
+            <a href="https://twitter.com/matthewmagee05">Twitter</a>
+          </ContactText>
+          <Link to="/blog/">
+            <Button marginTop="35px">Check Out My Blog</Button>
+          </Link>
+        </Inner>
+        <Footer></Footer>
+      </Contact>
+    </Parallax>
+  </>
+)
+
+export default Index
